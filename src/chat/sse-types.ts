557f9ca0820f91b2e818e2cee6a9ac6283f7b340
lanbery <cwd-pc@xy-info.com>
2025-02-10 09:58:07 +0800

    feat: update SSE message data chunk struct

--- src/chat/sse-types.ts
+++ src/chat/sse-types.ts
@@ -1,13 +1,18 @@
 /**
- * xai SSE message data struct
- * id is ai provider return chunk id
- * content is ai chunk content,like openai choices[0].delta.content
- * error: maybe after call ai chat.completion but does't connecting ai provider
+ * @public
+ *  xai SSE message data struct
+ * @property id is ai provider return chunk id
+ * @property content  is ai chunk content,like openai choices[0].delta.content
+ * @property finish_reason chunk choice finish_reason
+ *  with openai,deepseek will stop or null
+ * @property errcode: proxy logic transform error sign witch provider
+ *  maybe after call ai chat.completion but does't connecting ai provider
  *
  */
 export type SseMessageChunkData = {
   id?: string;
   content: string;
-  error?: string;
+  errcode?: string;
+  finish_reason?: string | null;
   [k: string]: any;
 };
